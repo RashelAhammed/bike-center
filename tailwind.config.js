276457d2cab2d9100c161e4328a2f05b4d7e426c
Rashel Ahammed <rasel01ahammed@gmail.com>
2023-07-22 23:21:38 +0600

    nave added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-          }
-        }
-      }
-    }
-  </script>
-</head>
-
-</html>
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
